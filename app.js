0089030ef465625e7b5854b540e7336a392fd3a9
Joe Lanman <joelanman@gmail.com>
2015-05-19 12:27:59 +0100

    pretty html

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.locals.pretty = true;
+
 app.use('/', routes);
 app.use('/users', users);
 
