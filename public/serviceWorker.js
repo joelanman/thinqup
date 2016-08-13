9c350fa1a907841f8b191ed7a831ccc17bbe249b
Joe Lanman <joelanman@gmail.com>
2016-08-13 20:18:34 +0100

    offline sort of working

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,19 +1,56 @@
-self.addEventListener('install', event => {
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('serviceworker-cache-polyfill.js');
 
-  function onInstall () {
-    return caches.open('static')
-      .then(cache => cache.addAll([
-        '/javascripts/pleasure-achievement.js',
-        '/stylesheets/style.css',
-        '/'
-      ])
-    );
-  }
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
+self.addEventListener('install', function(event) {
 
-  event.waitUntil(onInstall(event));
-	console.log('installing');
+	if (self.skipWaiting) { self.skipWaiting(); }
+
+  // We pass a promise to event.waitUntil to signal how
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+				'stylesheets/style.css',
+				'manifest.json',
+				'javascripts/pleasure-achievement.js',
+				'./pleasure-achievement-chart',
+				'./thought-record'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  // Do activate stuff: This will come later on.
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+	console.log('sw: fetch');
+	console.log(event.request.url);
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+	// `claim()` sets this worker as the active worker for all clients that
+	// match the workers scope and triggers an `oncontrollerchange` event for
+	// the clients.
+	return self.clients.claim();
 });
